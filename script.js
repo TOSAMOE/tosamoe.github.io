7ab06cc541db214c24bbfbff3fd2cd63de20ffc0
TOSAMOE <103938263+TOSAMOE@users.noreply.github.com>
2024-11-17 12:10:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 window.addEventListener('load', () => {
     const preloader = document.getElementById('preloader');
-    const blurOverlay = document.getElementById('blur-overlay');
-    const mainContent = document.querySelector('main');
-
-    // Убираем размытие и скрываем логотип через 3 секунды после загрузки
     setTimeout(() => {
-        blurOverlay.style.backdropFilter = 'blur(0px)';
         preloader.style.opacity = '0';
-
         setTimeout(() => {
             preloader.style.display = 'none';
-            mainContent.style.opacity = '1';
-        }, 1000);
+        }, 500);
     }, 3000);
 });
